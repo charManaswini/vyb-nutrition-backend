d4b5d31c5bdce45be2ad84ce959097971d5cf92e
Manaswini Poluri <110304039+charManaswini@users.noreply.github.com>
2025-04-30 22:44:28 +0530

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,25 @@
 def safe_get_weight(entry, fallback=50):
-    try:
-        return int(entry.get("weight_in_grams", fallback))
-    except:
-        return fallback
+    weight = entry.get("weight_in_grams", fallback)
+
+    if not isinstance(weight, (int, float)):
+        try:
+            weight = int(weight)
+        except:
+            weight = fallback
+
+    # conversion
+    if weight <= 10:
+        unit = "teaspoon"
+        approx_qty = round(weight / 5, 1)
+    elif weight <= 50:
+        unit = "tablespoon"
+        approx_qty = round(weight / 15, 1)
+    elif weight <= 180:
+        unit = "cup"
+        approx_qty = round(weight / 60, 1)
+    else:
+        unit = "grams"
+        approx_qty = weight
+
+    entry["friendly_quantity"] = f"{approx_qty} {unit}{'s' if approx_qty > 1 else ''} (≈ {weight}g)"
+    return weight
